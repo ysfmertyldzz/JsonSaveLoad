3876b1005b2d4df44a8224849c9a3d10503186a3
ysfmertyldzz <77334914+ysfmertyldzz@users.noreply.github.com>
2021-07-14 21:59:19 +0300

    JsonSaveLoad

---
+++ JSONSaveLoad/Assets/JSONSaveLoad.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONSaveLoad : MonoBehaviour
+{
+    public InventoryItems inventoryItems;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            SaveInventoryItems();
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            LoadInventoryItems();
+    }
+
+    public void SaveInventoryItems()
+    {
+        var data = JsonUtility.ToJson(inventoryItems);
+        PlayerPrefs.SetString("Data", data);
+
+        Debug.Log("Saved");
+    }
+
+    public void LoadInventoryItems()
+    {
+        inventoryItems = JsonUtility.FromJson<InventoryItems>(PlayerPrefs.GetString("Data"));
+
+        Debug.Log("Loaded");
+    }
+}
+
+[System.Serializable]
+public class InventoryItems
+{
+    public List<Item> items = new List<Item>();
+}
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public int itemDamage;
+}
